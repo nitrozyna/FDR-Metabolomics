d7cfdfb6e218071b199a4f378e86ad0f066481e6
nitrozyna <kurkiewicz.margaret@gmail.com>
2020-10-09 22:17:40 +0200

    Added a parser for passatuto .ms files

---
+++ src/main.py
@@ -0,0 +1,47 @@
+import os
+
+name_overwrites = {
+    "INCHI_AUX": "inchiaux",
+    "SpectrumFile": "source_file",
+    "LibraryName": "library_membership",
+    "compound": "compound_name",
+    "formula": "formula_smiles",
+    "ionization": "adduct",
+    "parentmass": "parent_mass"
+}
+
+cast_to = {
+    "parent_mass": float,
+    "charge": int,
+    "precursor_mz": float,
+}
+
+def parse_passatuto(ms_file):
+    json_format = {}
+    peaks = False
+    peaks_json = []
+    for line in open(ms_file):
+        if peaks:
+            peaks_json.append(list(map(float, line.split())))
+        elif line.startswith(">collision"):
+            peaks = True
+        elif line.strip():
+            key, value = line[1:].strip().split(' ', 1)
+            key = name_overwrites.get(key, key.lower())
+            if key in cast_to:
+                value = cast_to[key](value)
+            json_format[key] = value
+    json_format["peaks_json"] = peaks_json
+    return json_format
+
+def parse_passatuto_folder(folder_path):
+    spectrums = []
+    ms_files = os.listdir(folder_path)
+    for i, file in enumerate( ms_files ):
+        if file.endswith(".ms"):
+            spectrum = parse_passatuto(os.path.join(folder_path,file))
+            spectrums.append(spectrum)
+        if i and not i % 100:
+            print( "processed %d files" % i )
+    return spectrums
+
