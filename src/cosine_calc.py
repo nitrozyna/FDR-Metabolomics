c2dacc998aef8538ef4ea6c2778074554ed313c2
nitrozyna <kurkiewicz.margaret@gmail.com>
2021-03-28 22:39:35 +0200

    Changed lc-ms notebook

--- src/cosine_calc.py
+++ src/cosine_calc.py
@@ -12,8 +12,8 @@ Hit = namedtuple('Hit', ['query', 'target', 'score', 'hit'])
 def inchis_equal(s1, s2, spec2vec=False):
     meta1 = getMeta(s1, spec2vec)
     meta2 = getMeta(s2, spec2vec)
-    return meta1.get('inchikey_inchi', meta1.get('inchi', '')).split("-")[0] == \
-           meta2.get('inchikey_inchi', meta2.get('inchi', '')).split("-")[0]
+    return meta1.get('inchikey', meta1.get('inchikey_inchi', '')).split("-")[0] == \
+           meta2.get('inchikey', meta2.get('inchikey_inchi', '')).split("-")[0]
 
 
 def passatutto_inchis_equal(s1, s2, spec2vec=False):
