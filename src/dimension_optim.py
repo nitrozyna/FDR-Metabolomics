f553c15d7c7275b73e2d590b52d24a59a22bae2d
nitrozyna <kurkiewicz.margaret@gmail.com>
2021-04-09 08:49:08 +0200

    Refactored dimension_optim

--- src/dimension_optim.py
+++ src/dimension_optim.py
@@ -3,21 +3,16 @@ import gensim
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from matchms.filtering import normalize_intensities
 from matchms.importing import load_from_json
-from spec2vec.SpectrumDocument import SpectrumDocument
+from spec2vec import SpectrumDocument
 
+def post_process(s):
+    s = normalize_intensities(s)
+    # s = select_by_mz(s, mz_from=0, mz_to=1000)
+    # s = require_minimum_number_of_peaks(s, n_required=10)
+    return s
 
-json_file_name = 'C:\\Users\\Gosia\\Desktop\\FDR-datsets\\specs.json'
-spec_with_precursor = load_from_json(json_file_name)
-
-inchi_dict = {}
-for s in spec_with_precursor:
-    ik = s.metadata['inchikey_inchi']
-    init_ik = ik.split('-')[0]
-    if not init_ik in inchi_dict:
-        inchi_dict[init_ik] = [s]
-    else:
-        inchi_dict[init_ik].append(s)
 
 documents_query = []
 documents_lib = []
@@ -25,90 +20,87 @@ spectrums_lib = []
 spectrums_query = []
 
 
-def init(query_size=1500, library_size=2500, unseen_query_size=500, unique_match=True):
+def init(query_size=1000, min_identical_matches=1, spec2vec_decimal_places=2,
+         folder_name='C:\\Users\\Gosia\\Desktop'):
     global documents_query
     global documents_lib
     global spectrums_lib
     global spectrums_query
-    singletons = set([i for i, v in inchi_dict.items() if len(v) == 1])
-    multis = set([i for i, v in inchi_dict.items() if len(v) > 1])
 
-    matching_keys = np.random.choice(list(multis), size=query_size - unseen_query_size, replace=False)
-    other_keys = np.random.choice(list(singletons), size=library_size - query_size + 2 * unseen_query_size,
-                                  replace=False)
+    spec_with_precursor = load_from_json(
+        r'C:\Users\Gosia\Desktop\data\gnps_positive_ionmode_cleaned_by_matchms_and_lookups.json')
+    # apply post processing steps to the data
+    spec_with_precursor = [post_process(s) for s in spec_with_precursor if s.metadata.get('inchikey')]
+    # omit spectrums that didn't qualify for analysis
+    spec_with_precursor = [s for s in spec_with_precursor if s is not None]
 
-    query_spec = []
+    inchi_dict = {}
+    for s in spec_with_precursor:
+        ik = s.metadata['inchikey']
+        init_ik = ik.split('-')[0]
+        if not init_ik in inchi_dict:
+            inchi_dict[init_ik] = [s]
+        else:
+            inchi_dict[init_ik].append(s)
+
+    multis = set([i for i, v in inchi_dict.items() if len(v) > min_identical_matches])
+
+    matching_keys = np.random.choice(list(multis), size=query_size, replace=False)
+
+    query_spec = {}
     library_spec = []
+    # We select `query_size` queries that have at least `min_identical_matches` matching spectra in the library,
     for q in matching_keys:
-        ss = np.random.choice(inchi_dict[q], size=2, replace=False)
-        query_spec.append(ss[0])
-        if unique_match:
-            library_spec.append(ss[1])
-        else:
-            for s_ in ss[1:]:
-                library_spec.append(s_)
-    for i, o in enumerate(other_keys):
-        ss = np.random.choice(inchi_dict[o], size=1, replace=False)
-        if i < unseen_query_size:
-            query_spec.append(ss[0])
-        else:
-            library_spec.append(ss[0])
+        spec_to_add = np.random.choice(inchi_dict[q], size=1, replace=False)
+        query_spec[spec_to_add[0].metadata['spectrum_id']] = spec_to_add[0]
 
-    assert len(query_spec) == query_size
+    # And everything else goes into the library
+    for s in spec_with_precursor:
+        if s.metadata['spectrum_id'] not in query_spec:
+            library_spec.append(s)
 
     spectrums_lib = library_spec
-    spectrums_query = query_spec
-
-    documents_query = [SpectrumDocument(s, n_decimals=2) for s in spectrums_query]
-    documents_lib = [SpectrumDocument(s, n_decimals=2) for s in spectrums_lib]
-
-
-folder_name = 'C:\\Users\\Gosia\\Desktop'
-path_models = os.path.join(folder_name, "trained_models_1")
+    spectrums_query = list(query_spec.values())
+    documents_lib = [SpectrumDocument(s, n_decimals=spec2vec_decimal_places) for s in spectrums_lib]
+    documents_query = [SpectrumDocument(s, n_decimals=spec2vec_decimal_places) for s in spectrums_query]
 
 
-def f(size, intensity_weighting_power=0.5, allowed_missing_percentage=15):
-    #model_file = os.path.join(path_models, "spec2vec_librarymatching_size_%d.model" % size)
-    iterations = [1, 3, 5, 10]
-
-    # Train model with size 10 and default parameters
-    from spec2vec.vector_operations import calc_vector
-    model_file = os.path.join(path_models, "spec2vec_size_%d.model"%size)
+def calculate_thresholds(model_file, intensity_weighting_power, allowed_missing_percentage, cos_min_match=6):
+    print('using model %s' % model_file)
 
     # Load pretrained model
     model = gensim.models.Word2Vec.load(model_file)
+    vector_size = model.vector_size
 
-    cos_hits, _ = cosine_calc.get_hits(spectrums_query, spectrums_lib, cosine_tol=.005)
-    hits, _ = cosine_calc.get_hits(documents_query, documents_lib, spec2vec_model=model,
-                                   intensity_weighting_power=intensity_weighting_power,
-                                   allowed_missing_percentage=allowed_missing_percentage)
-
-    test_matches_cos = []
-    test_matches_s2v = []
-
-    thresholds = np.array([x / 20 for x in range(0,20)]+[.98,.99,.997,.999])
-
-    # for every query
-    # find its 'hit'
-    # if True match, 1
-    # if False match, 0
-    # otherwise, -1
+    cos_hits = cosine_calc.get_hits(spectrums_query, spectrums_lib, cosine_tol=.005, min_match_count=cos_min_match)
+    hits = cosine_calc.get_spec2vec_hits(documents_query, documents_lib, model=model,
+                                         intensity_weighting_power=intensity_weighting_power,
+                                         allowed_missing_percentage=allowed_missing_percentage)
 
     cos_hit_dict = {hit.query.metadata['spectrum_id']: hit for hit in cos_hits}
     hit_dict = {hit.query: hit for hit in hits}
 
+    test_matches_cos = []
+    test_matches_s2v = []
+    thresholds = np.array([x / 20 for x in range(20)] + [.98])
     for threshold in thresholds:
-        print(f"Checking matches for spec2vec score > {threshold:.2f}")
+        print(f"Checking matches for similarity score > {threshold:.2f}")
         test_matches = []
+        # for every query
         for spec in spectrums_query:
-            if spec.metadata['spectrum_id'] in cos_hit_dict and cos_hit_dict[spec.metadata['spectrum_id']].score > threshold:
+            # find its 'hit'
+            if spec.metadata['spectrum_id'] in cos_hit_dict and cos_hit_dict[
+                spec.metadata['spectrum_id']].score > threshold:
                 test_matches.append(1 * cos_hit_dict[spec.metadata['spectrum_id']].hit)
             else:
                 test_matches.append(-1)
-        # Make arrays from lists:
+        # if True match, 1
+        # if False match, 0
+        # otherwise ('decoy'), -1
         test_arr = np.array(test_matches)
         test_matches_cos.append([np.sum(test_arr == 1), np.sum(test_arr == 0), np.sum(test_arr == -1)])
 
+        # Then the repeat the same procedure for Spec2Vec matches
         test_matches = []
         for doc in documents_query:
             if doc in hit_dict and hit_dict[doc].score > threshold:
@@ -116,17 +108,15 @@ def f(size, intensity_weighting_power=0.5, allowed_missing_percentage=15):
                 test_matches.append(1 * hit.hit)
             else:
                 test_matches.append(-1)
-
-        # Make arrays from lists:
         test_arr = np.array(test_matches)
         test_matches_s2v.append([np.sum(test_arr == 1), np.sum(test_arr == 0), np.sum(test_arr == -1)])
 
-    min_match = 6
+    # Convert lists to np.array
     test_matches_cos_arr = np.array(test_matches_cos)
     test_matches_s2v_arr = np.array(test_matches_s2v)
 
-    label_picks = [0, 4, 8, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-
+    # Plot the outcome
+    label_picks = [0, 4, 8, 12, 16, 18, 20]
     plt.figure(figsize=(7, 6))
     plt.style.use('ggplot')
     num_max_cos = np.sum(test_matches_cos_arr[0, :])
@@ -145,19 +135,21 @@ def f(size, intensity_weighting_power=0.5, allowed_missing_percentage=15):
                          textcoords="offset points", xytext=(2, -10), fontsize=12)
             plt.annotate(">{:.2}".format(threshold),
                          (test_matches_s2v_arr[i, 1] / num_max, test_matches_s2v_arr[i, 0] / num_max),
-                         textcoords="offset points", xytext=(2, -10), fontsize=12)
+                         textcoords="offset points", xytext=(2, -10), fontsize=12, color='red')
 
-    # plt.plot(test_matches_ROC_min2_arr[:,1]/num_max, test_matches_ROC_min2_arr[:,0]/num_max,
-    #         '.--', color='black', alpha=0.5, label='cosine (min match = 2)')
-
-    plt.title('true/false positives per query')
+    plt.title('true/false positives per threshold')
     plt.legend(fontsize=14)
     plt.xticks(fontsize=13)
     plt.yticks(fontsize=13)
-    plt.xlabel('false positives rate', fontsize=16)
+    plt.xlabel('false positive rate', fontsize=16)
     plt.ylabel('true positive rate', fontsize=16)
-    plt.xlim(0,.8)
-    plt.ylim(0,.5)
+    plt.xlim(0, .35)
+    plt.ylim(.1, .8)
 
-    filename2 = "tfr_%d_%.2f_%.2f.png" % (size, intensity_weighting_power, allowed_missing_percentage)
-    plt.savefig(os.path.join(folder_name, 'FDR-Metabolomics', 'data', 'opt_lcms_dim', filename2))
+    # And save it down so that we can compare multiple plots more easily
+    filename = "%s.png" % model_file
+    folder_to_save = os.path.join(folder_name, 'opt_lcms_dim')
+    os.mkdir(folder_to_save)
+    filepath = os.path.join(folder_to_save, filename)
+    plt.savefig(filepath)
+    print('saved outcome to %s' % filepath)
